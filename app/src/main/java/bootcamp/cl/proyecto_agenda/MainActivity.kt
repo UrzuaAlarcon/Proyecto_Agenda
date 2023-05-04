b917034bd1af3a36a432b062a256ebdf3c1a0a2a
UrzuaAlarcon <122647461+UrzuaAlarcon@users.noreply.github.com>
2023-05-04 10:50:05 -0400

    Initial commit

---
+++ app/src/main/java/bootcamp/cl/proyecto_agenda/MainActivity.kt
@@ -0,0 +1,16 @@
+package bootcamp.cl.proyecto_agenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
