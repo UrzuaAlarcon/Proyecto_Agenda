e540e2fa640ffcbc666ab8437927c1645e922f4c
UrzuaAlarcon <122647461+UrzuaAlarcon@users.noreply.github.com>
2023-05-04 13:09:20 -0400

    Intento 1 botones

--- app/src/main/java/bootcamp/cl/proyecto_agenda/Home.kt
+++ app/src/main/java/bootcamp/cl/proyecto_agenda/Home.kt
@@ -1,32 +1,28 @@
 package bootcamp.cl.proyecto_agenda
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import bootcamp.cl.proyecto_agenda.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Home : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private lateinit var dataBinding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        dataBinding= DataBindingUtil.setContentView(Activity(), R.layout.fragment_home)
+
     }
 
     override fun onCreateView(
@@ -35,25 +31,33 @@ class Home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState);
+
+        var btnLogin:Button = dataBinding.btnLogin
+        var btnCreateAccount:Button = dataBinding.btnCreateAccount
+
+        val navControler:NavController = Navigation.findNavController(view)
+
+        btnLogin.setOnClickListener {
+
+
+                navControler.navigate(R.id.login2)
+
+        }
+
+        btnCreateAccount.setOnClickListener {
+
+            navControler.navigate(R.id.createAccount2)
+
+        }
+
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Home().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
